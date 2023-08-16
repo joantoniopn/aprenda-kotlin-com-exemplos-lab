320937258c500136b855ba8205a33ba86e39b714
Joao Neto <joao-antoniopn@live.com>
2023-08-16 19:35:45 -0300

    Finalizado o desafio 2

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,74 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+class Usuario (val nome: String, val idade: Int) {
+    override fun toString(): String {
+        return "Usuario: " + this.nome + " | " + "Idade: " + this.idade
     }
 }
 
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60) {
+    override fun toString(): String {
+        return "Curso: " + this.nome + " - " + "Duracao: " + this.duracao
+    }
+}
+
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
+
+    val inscritos = mutableListOf<Usuario>();
+
+    fun matricular(usuario: Usuario) {
+        inscritos.add(usuario)
+    }
+
+    override fun toString(): String  {
+        return "Formacao: " + this.nome + "\nInscritos: " + this.inscritos.count() + "\nDificuldade: " + this.nivel
+    }
+
+    fun listarConteudos() {
+        print("\n----------------------------------\n")
+        this.conteudos.forEach {
+                i -> println(i)
+        }
+    }
+
+    fun listarInscritos() {
+        print("----------------------------------\n")
+        this.inscritos.forEach {
+                i -> println(i)
+        }
+    }
+
+    fun removerMatricula(usuario: Usuario) {
+        inscritos.removeIf { it.nome === usuario.nome }
+    }
+}
+
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1: Usuario = Usuario("Joao", 29);
+    val usuario2: Usuario = Usuario("Jorge", 30);
+    val usuario3: Usuario = Usuario("Luana", 26);
+
+    val systemUsers: MutableList<ConteudoEducacional> = mutableListOf(
+        ConteudoEducacional("Logica com Kotlin", 20),
+        ConteudoEducacional("Configurando Ambiente", 4),
+        ConteudoEducacional("Iniciando um projeto", 30)
+    )
+
+    val conteudos: List<ConteudoEducacional> = systemUsers;
+
+    val Formacao1: Formacao = Formacao("Kotlin iniciante", conteudos, Nivel.BASICO);
+    Formacao1.matricular(usuario1);
+    Formacao1.matricular(usuario2);
+    Formacao1.matricular(usuario3);
+
+    print(Formacao1);
+    Formacao1.listarConteudos()
+    Formacao1.listarInscritos()
+
+    Formacao1.removerMatricula(usuario1)
+
+    Formacao1.listarInscritos()
 }
